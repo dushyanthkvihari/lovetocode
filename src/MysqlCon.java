e3a1ec1210c5be822b579576af2393de5580c115
dushyanthkvihari <59311496+dushyanthkvihari@users.noreply.github.com>
2019-12-28 18:54:09 +0530

    Update MysqlCon.java

--- src/MysqlCon.java
+++ src/MysqlCon.java
@@ -4,7 +4,7 @@ public static void main(String args[]){
 try{  
 Class.forName("com.mysql.jdbc.Driver");  
 Connection con=DriverManager.getConnection(  
-"jdbc:mysql://10.96.152.121:3306/test_chinese?useUnicode=yes&characterEncoding=UTF-8","root","Opentext1!");
+"jdbc:mysql://hotst:3306/test_chinese?useUnicode=yes&characterEncoding=UTF-8","root","***");
 //here sonoo is database name, root is username and password
 Statement stmt1 = con.createStatement();
 stmt1.executeUpdate("insert into emp(name) value ('喜歡');");
